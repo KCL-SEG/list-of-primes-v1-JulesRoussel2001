a81c14d040182a853a9c4d739ee1dfcff266d88c
Jules Roussel <k20074057@kcl.ac.uk>
2022-10-04 16:30:37 +0100

    All tests passed for prime v1

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        count = 0
+        for i in range(1,num):
+            if num % i == 0:
+                count += 1
+        if count == 1:
+            list.append(num)
+        num += 1
     return list
